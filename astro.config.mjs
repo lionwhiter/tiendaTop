53ac9622fc5d906cefe5e13a891baf57d748981c
lionwhiter <yolbysalonso94@gmail.com>
2026-06-28 23:25:20 -0400

    FIX-Mod correccion par github Pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import tailwindcss from '@tailwindcss/vite'
 
 export default {
-  site: 'https://tiendaTop.github.io',
+  // El dominio de GitHub Pages, NO el link al repositorio
+  site: 'https://lionwhiter.github.io', 
+  
+  // Debe coincuir EXACTAMENTE con el nombre de tu repositorio (respetando mayúsculas/minúsculas)
+  base: '/tiendaTop/', 
+  
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
